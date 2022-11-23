6e810e265ffd380f860114c8ceafce787194af60
huangxingbo <hxb@apache.org>
2022-11-23 16:57:05 +0800

    fix

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ html_static_path = ['_static']
 
 html_theme_options = {
     "switcher": {
-        "json_url": "https://github.com/HuangXingBo/pyflink-docs-2/tree/test/docs/_static/switcher.json",
+        "json_url": "https://pyflink-docs-test2.readthedocs.io/en/latest/_static/switcher.json",
         "version_match": flink_doc_version,
     },
     "navbar_end": ["version-switcher"]
